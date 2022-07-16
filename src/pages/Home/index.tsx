17bbedef6e3a14b362317f5c3b7966738bffe96a
mtheusbrito <matheusbritodasilva@gmail.com>
2022-07-15 23:08:41 -0300

    Validando Formulários

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   CountDownContainer,
   FormContainer,
@@ -11,13 +14,29 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 // controlled -> useState/input
 // uncontrolled
 const Home: React.FC = () => {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
@@ -46,7 +65,6 @@ const Home: React.FC = () => {
             type="number"
             id="minutesAmount"
             min={5}
-            max={60}
             step={5}
             placeholder="00"
             {...register('minutesAmount', { valueAsNumber: true })}
